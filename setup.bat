2a8bb655cc781c02755e79ddf9c9e5f99611256c
Wahid <wahidb@gmail.com>
2017-07-02 18:27:05 +0200

    Fixed noob wadii's whore code hhh

---
+++ setup.bat
@@ -0,0 +1,7 @@
+git clone https://github.com/adafruit/Adafruit_Python_GPIO.git
+cd Adafruit_Python_GPIO
+python setup.py install
+cd ..
+git clone https://github.com/adafruit/Adafruit_Python_BME280.git
+cd Adafruit_Python_BME280
+python setup.py install
