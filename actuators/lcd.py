45936dfdd4f77ce8267e837fcd1ec8632cf00411
liquidwad <wadii.bellamine@gmail.com>
2017-07-09 19:13:44 +0000

    Fixing latest change

--- actuators/lcd.py
+++ actuators/lcd.py
@@ -30,7 +30,7 @@ class RelayLCD(CloudLCD):
         self.reportAvailability(True, data)
         self.data = data
 	
-    def getDisplayString(data):
+    def getDisplayString(self,data):
         # We have 16 chars per row times 2 rows
         # Each sub-string is max 7 chars
         w = 7
