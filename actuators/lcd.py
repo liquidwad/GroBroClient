1c73fa1f3b8c8f23eff84b44dcb88828f5b5cd7a
liquidwad <wadii.bellamine@gmail.com>
2017-07-09 01:55:36 +0000

    Added unit test for LCD

--- actuators/lcd.py
+++ actuators/lcd.py
@@ -3,9 +3,9 @@ from cloud import *
 from config import *
 
 class CloudLCD(CloudActuator):
-    def __init__(self, name, cloud, address, defaultString = ""):
+    def __init__(self, name, cloud, addr, defaultString = ""):
         CloudActuator.__init__(self, name, cloud)
-        self.lcd = CharLCD(address = address, port = 1, cols = 16, rows = 2, dotsize = 8, charmap = 'A02')
+        self.lcd = CharLCD(address = addr, port = 1, cols = 16, rows = 2, dotsize = 8, charmap = 'A02')
         self.write(defaultString)
         self.reportAvailability(True)
 	
@@ -23,3 +23,7 @@ class CloudLCD(CloudActuator):
 		
 		if((data.value is not None) and (data.value is not self.state)):
 		    self.write(data.value)
+		    
+if __name__ == "__main__":
+    testLCD = CharLCD(address = LCD_LEFT_ADDR, port = 1, cols = 16, rows = 2, dotsize = 8, charmap = 'A02')
+    testLCD.write_string("Test")
